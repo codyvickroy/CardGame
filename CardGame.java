5f223ff688a6531859a0f45c99844ee1296113bf
codyvickroy <codyvickroy@gmail.com>
2013-12-05 22:58:51 -0500

    Added the GUI, CW, and

--- CardGame.java
+++ CardGame.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cardgame;
-
 /**
- *
- * @author codyvickroy
+ *  Cody Vickroy
+ *  Java 110 | UVM
+ *  December 7th, 2013
+ * 
+ *  Its a game of cards, okay.
  */
+
 public class CardGame {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+    
+}//end class CardGame
---
+++ CardGameCW.java
@@ -0,0 +1,11 @@
+/**
+ *  Cody Vickroy
+ *  Java 110 | UVM
+ *  December 7th, 2013
+ * 
+ *  The command window for a Card Game
+ */
+
+public class CardGameCW {
+    
+}
